4f58219d13590cb27c450212f7e205c99497818b
ObiFaith <obifth@gmail.com>
2024-08-01 09:00:54 +0100

    feat: add-the-functionalities-of-the-ecommerce-page

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const qty = document.querySelector('.qty');
-const nav = document.querySelector('.nav');
+const modal = document.querySelector('.modal');
+const navSm = document.querySelector('.nav-sm');
 const menu = document.querySelector('.logo img');
-const lightBox = document.querySelector('.sneaker');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
 const cartIcon = document.querySelector('cart-icon');
 const addToCart = document.querySelector('.addOn-cart');
+const header = document.querySelector('header').firstElementChild;
 const prodQty = document.querySelector('.addOn .text-dark--blue');
+const prevBtns = Array.from(document.querySelectorAll('.prev-btn'));
+const nextBtns = Array.from(document.querySelectorAll('.next-btn'));
+const lightBoxs = Array.from(document.querySelectorAll('.sneaker'));
 const cartBtns = Array.from(document.querySelectorAll('.addOn .text-orange'));
 const thumbnailImgs = Array.from(document.querySelectorAll('.thumbnails img'));
-
-console.log(menu);
 
 const setActiveThumbnail = thumbnailImg => {
 	thumbnailImgs.forEach(img => img.classList.remove('active'));
 	thumbnailImg.classList.add('active');
-	lightBox.src = thumbnailImg.src.replace('-thumbnail', '');
+	lightBoxs.forEach(
+		lightBox => (lightBox.src = thumbnailImg.src.replace('-thumbnail', ''))
+	);
 };
+
+lightBoxs[0].addEventListener('click', () => {
+	modal.style.display = 'grid';
+});
 
 thumbnailImgs.forEach(thumbnailImg => {
 	thumbnailImg.addEventListener('click', () =>
@@ -27,17 +33,21 @@ thumbnailImgs.forEach(thumbnailImg => {
 const findActiveIndex = () =>
 	thumbnailImgs.findIndex(img => img.classList.contains('active'));
 
-prevBtn.addEventListener('click', () => {
-	let activeIndex = findActiveIndex();
-	if (!activeIndex) activeIndex = thumbnailImgs.length;
-	setActiveThumbnail(thumbnailImgs[activeIndex - 1]);
-});
+prevBtns.forEach(prevBtn =>
+	prevBtn.addEventListener('click', () => {
+		let activeIndex = findActiveIndex();
+		if (!activeIndex) activeIndex = thumbnailImgs.length;
+		setActiveThumbnail(thumbnailImgs[activeIndex - 1]);
+	})
+);
 
-nextBtn.addEventListener('click', () => {
-	let activeIndex = findActiveIndex();
-	if (activeIndex === thumbnailImgs.length - 1) activeIndex = -1;
-	setActiveThumbnail(thumbnailImgs[activeIndex + 1]);
-});
+nextBtns.forEach(nextBtn =>
+	nextBtn.addEventListener('click', () => {
+		let activeIndex = findActiveIndex();
+		if (activeIndex === thumbnailImgs.length - 1) activeIndex = -1;
+		setActiveThumbnail(thumbnailImgs[activeIndex + 1]);
+	})
+);
 
 cartBtns.forEach(btn => {
 	btn.addEventListener('click', () => {
@@ -49,11 +59,31 @@ cartBtns.forEach(btn => {
 });
 
 addToCart.addEventListener('click', () => {
-	qty.style.display = 'inline';
-	qty.textContent = prodQty.textContent;
+	if (prodQty.textContent > 0) {
+		qty.style.display = 'inline';
+		qty.textContent = prodQty.textContent;
+	}
 });
 
 menu.addEventListener('click', () => {
-  nav.style.display = 'block';
-  const nav_list = nav.querySelector('.nav_list');
-})
+	if (menu.src.includes('menu')) {
+		navSm.style.display = 'block';
+		header.style.alignItems = 'flex-start';
+		header.parentElement.style.paddingBottom = '1rem';
+		menu.src = menu.src.replace('menu', 'close');
+		const nav_list = navSm.querySelector('.nav_list');
+		nav_list.parentElement.style.paddingBottom = '1rem';
+		nav_list.style.width = '100%';
+		nav_list.style.textAlign = 'center';
+	} else {
+		nav.style.display = 'none';
+		menu.src = menu.src.replace('close', 'menu');
+	}
+});
+
+modal.addEventListener('click', event => {
+	const modalBox = modal.firstElementChild;
+	if (!modalBox.contains(event.target)) {
+		modal.style.display = 'none';
+	}
+});
